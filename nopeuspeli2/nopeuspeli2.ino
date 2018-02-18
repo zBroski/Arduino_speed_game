695ec28e0e1753e3751077ed8912302e3bd43ae3
aito_broski <timo.mauranen@aalto.fi>
2018-02-19 00:25:38 +0200

    Update nopeuspeli2.ino

--- nopeuspeli2/nopeuspeli2.ino
+++ nopeuspeli2/nopeuspeli2.ino
@@ -1,8 +1,9 @@
 int randNum; 
-short int i,point,j;
-bool k= true;                       
+short int points,j;
+int Delay = 600;
+bool k = true;                       
 
-void setup() {
+void setup(){
   randomSeed(analogRead(0));
   Serial.begin(9600);
   pinMode(3, OUTPUT); 
@@ -13,8 +14,8 @@ void setup() {
   pinMode(2, INPUT);
   attachInterrupt(digitalPinToInterrupt(2), point_count, RISING);      //Setting the external interrupt on pin 2 raising edge
 }
-void start_routine()                                      //Three blink routine to indicate the start of the game
-{
+
+void start_routine(){
 /* for(i=0;i<=1;i++){
   digitalWrite(3,HIGH);
   delay(150);
@@ -28,26 +29,27 @@ void start_routine()                                      //Three blink routine 
  delay(2000);
  Serial.println("GO");
 }
-void loop() {
-  start_routine();                 //Calling game start routine
-    while(k){                       //Starting game sequence
-      randNum = random(3,7);
-      digitalWrite(randNum,HIGH);
-      delay(600);
-      digitalWrite(randNum,LOW);
-      delay(150);
-      j++;
-      if(j!=point){
-        k=false;
-      }  
-   }
-   Serial.print(point);          //Printing score in the serial monitor at the end of game
-   //Serial.println("/15 correct");
-   while(1);
+
+void loop(){
+  start_routine();                 
+  while(k){                       //Starting game sequence
+    randNum = random(3,7);
+    digitalWrite(randNum,HIGH);
+    delay(Delay);           
+    digitalWrite(randNum,LOW);
+    delay(150);
+    j++;
+    Delay = Delay - 5;
+    if(j!=points){
+      k=false;
+    }  
+  }
+  Serial.println(points);          //Printing score in the serial monitor at the end of game
+  while(1);
 }
 
 void point_count(){              //Couting point when interrupt encounters
   digitalWrite(randNum,LOW);
-  point=point+1;
+  points++;
 }
 
